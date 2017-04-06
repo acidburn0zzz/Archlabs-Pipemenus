4f79c005c7c0901432297c8708b90b41dd1d8891
Erik Dubois <erik.dubois@gmail.com>
2017-04-06 22:51:02 +0200

    delete calamares in tint2 and fix for obmenu

--- cleanup.sh
+++ cleanup.sh
@@ -3,6 +3,6 @@ rm -f /etc/sudoers.d/g_wheel
 rm -R /etc/systemd/system/getty@tty1.service.d
 rm /etc/systemd/system/default.target
 rm /usr/share/applications/calamares.desktop
-
-
-
+sed -i 's/exec openbox-session/#exec openbox-session/' ~/.xinitrc
+sed -i 's/#exec openbox-session/exec openbox-session/' ~/.xsession
+sed -i '/launcher_item_app = \/usr\/share\/applications\/calamares.desktop/d' ~/.config/tint2/tint2rc
