a0626b2772ccd4ed91c0f031114df85deb67bbd4
Erik Dubois <erik.dubois@gmail.com>
2017-04-15 21:06:51 +0200

    cleanup.sh put correct again

--- cleanup.sh
+++ cleanup.sh
@@ -4,6 +4,4 @@ rm -R /etc/systemd/system/getty@tty1.service.d
 rm /etc/systemd/system/default.target
 rm /usr/share/applications/calamares.desktop
 rm -rf /abif-master
-sed -i 's/exec openbox-session/#exec openbox-session/' ~/.xinitrc
-sed -i 's/#exec openbox-session/exec openbox-session/' ~/.xsession
-sed -i '/launcher_item_app = \/usr\/share\/applications\/calamares.desktop/d' ~/.config/tint2/tint2rc
+sed -i '/launcher_item_app = \/usr\/share\/applications\/calamares.desktop/d' /etc/skel/.config/tint2/tint2rc
