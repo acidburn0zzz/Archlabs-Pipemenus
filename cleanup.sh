bbda86d513d62d6989b37518c0dd1e6e3f708794
Erik Dubois <erik.dubois@gmail.com>
2017-04-17 18:58:01 +0200

    added a line to get root permission prompt on mintstick and pamacupdates

--- cleanup.sh
+++ cleanup.sh
@@ -4,4 +4,5 @@ rm -R /etc/systemd/system/getty@tty1.service.d
 rm /etc/systemd/system/default.target
 rm /usr/share/applications/calamares.desktop
 rm -rf /abif-master
+rm -f /etc/polkit-1/rules.d/49-nopasswd_global.rules
 sed -i '/launcher_item_app = \/usr\/share\/applications\/calamares.desktop/d' /etc/skel/.config/tint2/tint2rc
