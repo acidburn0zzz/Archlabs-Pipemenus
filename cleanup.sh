6d0a0eb3b69b691927ea71ed203b30cdb663762c
Erik Dubois <erik.dubois@gmail.com>
2017-04-21 19:22:07 +0200

    removing calamares in /etc and /usr/lib

--- cleanup.sh
+++ cleanup.sh
@@ -3,6 +3,8 @@ rm -f /etc/sudoers.d/g_wheel
 rm -R /etc/systemd/system/getty@tty1.service.d
 rm /etc/systemd/system/default.target
 rm /usr/share/applications/calamares.desktop
+rm -rf /etc/calamares
+rm -rf /usr/lib/calamares
 rm -rf /abif-master
 rm -f /etc/polkit-1/rules.d/49-nopasswd_global.rules
 sed -i '/launcher_item_app = \/usr\/share\/applications\/calamares.desktop/d' /etc/skel/.config/tint2/tint2rc
