922c0e004a60793aec415af1f394fc8e65138968
Erik Dubois <erik.dubois@gmail.com>
2017-04-24 23:13:55 +0200

    adding leftovers to delete

--- cleanup.sh
+++ cleanup.sh
@@ -9,3 +9,9 @@ rm -rf /abif-master
 rm -f /etc/polkit-1/rules.d/49-nopasswd_global.rules
 rm -f /etc/skel/.config/autostart/calamares.desktop
 sed -i '/launcher_item_app = \/usr\/share\/applications\/calamares.desktop/d' /etc/skel/.config/tint2/tint2rc
+systemctl disable pacman-init.service choose-mirror.service
+rm -r /etc/systemd/system/{choose-mirror.service,pacman-init.service,etc-pacman.d-gnupg.mount,getty@tty1.service.d}
+rm /etc/systemd/scripts/choose-mirror
+rm /root/{.automated_script.sh,.zlogin}
+rm /etc/mkinitcpio-archiso.conf
+rm -r /etc/initcpio
